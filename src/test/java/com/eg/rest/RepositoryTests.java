8bae7634333f830a3fad308cca53a5b52521c708
eg <eg@dartit.ru>
2019-06-04 15:53:53 +0500

    тест на последовательность

--- src/test/java/com/eg/rest/RepositoryTests.java
+++ src/test/java/com/eg/rest/RepositoryTests.java
@@ -1,5 +1,7 @@
 package com.eg.rest;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,6 +52,21 @@ public class RepositoryTests {
         repository.createWidget(1, 1, 100, 100, 1);
         repository.createWidget(1, 1, 100, 100, 2);
         Assert.assertTrue(repository.getAllWidgets().size() == 2);
+    }
+
+    @Test
+    public void testWidgetSequence() {
+        repository.clear();
+        repository.createWidget(1, 1, 100, 100, 1); //id 1, zIndex 2
+        repository.createWidget(1, 1, 100, 100, 2); //id 2, zIndex 3
+        repository.createWidget(1, 1, 100, 100, 3); //id 3, zIndex 4
+        repository.createWidget(1, 1, 100, 100, 1); //id 4, zIndex 1
+
+        List<WidgetModel> widgets = repository.getAllWidgets();
+        Assert.assertTrue(widgets.get(0).getId() == 4);
+        Assert.assertTrue(widgets.get(1).getId() == 1);
+        Assert.assertTrue(widgets.get(2).getId() == 2);
+        Assert.assertTrue(widgets.get(3).getId() == 3);
     }
 
 }
